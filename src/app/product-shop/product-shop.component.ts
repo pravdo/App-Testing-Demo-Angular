2e8b41a2b412a0acc3a11c8e025ca2e846a9851b
Pravdomir Kostov <pravdomir1998@gmail.com>
2024-02-14 22:08:06 +0200

    navbar and cart initial logic

--- src/app/product-shop/product-shop.component.ts
+++ src/app/product-shop/product-shop.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from 'src/data/products';
+import { Product } from 'src/product';
 
 @Component({
   selector: 'app-product-shop',
   templateUrl: './product-shop.component.html',
-  styleUrls: ['./product-shop.component.css']
+  styleUrls: ['./product-shop.component.css'],
 })
-export class ProductShopComponent {
+export class ProductShopComponent implements OnInit {
+  product!: Product | undefined;
+  disabledButton: boolean = false;
+  submitted: boolean = false;
+  text: string = 'Add to Cart';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const id = Number(routeParams.get('productId'));
+    this.product = products.find((product) => product.id === id);
+  }
+
+  addToCart() {
+    this.disabledButton = true;
+    this.text = 'Added to Cart';
+    this.submitted = true;
+  }
 }
