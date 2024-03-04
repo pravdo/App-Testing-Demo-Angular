3fb7a2960892ffea16907729009cf9137efb98eb
Pravdomir Kostov <pravdomir1998@gmail.com>
2024-03-04 19:31:50 +0200

    form submit integration

--- src/app/checkoutform/checkoutform.component.spec.ts
+++ src/app/checkoutform/checkoutform.component.spec.ts
@@ -1,23 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CheckoutformComponent } from './checkoutform.component';
+import { Contact } from '../Contact';
+import { FormsModule } from '@angular/forms';
 
 describe('CheckoutformComponent', () => {
   let component: CheckoutformComponent;
   let fixture: ComponentFixture<CheckoutformComponent>;
+  const formData: Contact = {
+    firstName: 'Pravdo',
+    lastName: 'K',
+    email: 'test@test.com',
+    password: '2345',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CheckoutformComponent ]
-    })
-    .compileComponents();
+      imports: [FormsModule],
+      declarations: [CheckoutformComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CheckoutformComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('form submit', () => {
     expect(component).toBeTruthy();
+    expect(component.submitted).toBe(false);
+    component.submit(formData);
+    fixture.detectChanges();
+    expect(component.submitted).toBe(true);
   });
 });
